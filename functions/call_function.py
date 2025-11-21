3b18c99a5274a2c562cc7ef053b9cbf38e605cd7
orcen <orcener@gmail.com>
2025-11-21 17:44:06 +0100

    AI model

---
+++ functions/call_function.py
@@ -0,0 +1,52 @@
+from google.genai import types
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+import os
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_dict = {
+        "write_file": write_file,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "get_file_content": get_file_content,
+    }
+
+    cwd = os.getcwd()
+    function_name = function_call_part.name
+    if function_name not in function_dict:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = function_dict[function_name] = function_dict[function_name](cwd, **function_call_part.args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
+        ]
+    )
